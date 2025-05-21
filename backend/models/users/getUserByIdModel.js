34c56d8aca7e5953764f102f9346ff676191759a
Simon Riedinger <riedinger.simon@t-online.de>
2025-05-21 15:24:03 +0200

    resolve merge conflict

--- backend/models/users/getUserByIdModel.js
+++ backend/models/users/getUserByIdModel.js
@@ -2,30 +2,22 @@ const db = require('../../db/database');
 
 async function getUserByIdModel(id) {
   const result = await db.query(
-<<<<<<< HEAD
-    `SELECT u.id, u.first_name, u.last_name, u.username, u.email, u.street, u.house_number, u.postal_code, u.city, u.country, r.name AS role
-    FROM users u
-    JOIN roles r
-    ON r.id = u.role_id
-    WHERE u.id = $1`,
-=======
-    `SELECT 
-        u.first_name, 
-        u.last_name, 
-        u.username, 
-        u.email, 
-        u.street, 
-        u.house_number, 
-        u.postal_code, 
-        u.city, 
-        u.country, 
+    `SELECT
+        u.first_name,
+        u.last_name,
+        u.username,
+        u.email,
+        u.street,
+        u.house_number,
+        u.postal_code,
+        u.city,
+        u.country,
         r.name AS role,
         s.name AS status
      FROM users u
      JOIN roles r ON r.id = u.role_id
      JOIN user_status s ON s.id = u.status_id
      WHERE u.id = $1`,
->>>>>>> dev
     [id]
   );
   return result.rows[0] || null;
