11bcbee379b2c205909d6009bbd3fa851d664548
Simon Riedinger <riedinger.simon@t-online.de>
2025-06-03 16:02:36 +0200

    important cors change!!!!

--- backend/utils/cors.js
+++ backend/utils/cors.js
@@ -1,6 +1,6 @@
 // backend/utils/cors.js
 function setCorsHeaders(res) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost'); // evtl. später auf env umstellen
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1337'); // evtl. später auf env umstellen
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
